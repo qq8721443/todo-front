625fe7f4d26969cd29e5fda7d49149aaee425f1c
qq8721443 <qq8721443@naver.com>
2020-09-11 21:47:24 +0900

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,8 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function(app){
     app.use(
-        createProxyMiddleware('/api',{
+        '/api',
+        createProxyMiddleware({
             target:'https://nayoung-todo-backend.herokuapp.com',
             changeOrigin:true
         })
